34d654c7112f41c8768d9c3daa86110a56a790dd
Andrew Neeley <aneeley05@gmail.com>
2022-06-30 09:11:09 -0700

    asdf

--- js/background.js
+++ js/background.js
@@ -4,11 +4,31 @@ setInterval(() => {
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].setAttribute("autocomplete", "no-autofill")
     }
-    //hideAllDeleteButtons();
+    makeMatterButtons();
 }, 1000)
 
+var deleteEnabled = false;
+
+const toggleDeleteEnabled = () => {
+    deleteEnabled = !deleteEnabled;
+}
+
+
+let allowDeleteButtonHTML = `<div id="injected" class="oE-YQbIeEsQILk3Eiv9Ir pt-popover-dismiss" style="height: 35px;background: #ff00003b;cursor: pointer;justify-content: center;align-items: center;color: #000000ad;display: flex;border-top: solid;border-width: 2px;"><i class="icon-warning _119eS8Qie5Aln0TDq33V5K"></i>Toggle Delete</div>`
+
 function callbackBoi(records) {
-    hideAllDeleteButtons();
+    if (document.readyState != "loading") {
+        hideAllDeleteButtons();
+        makeMatterButtons();
+
+        var popdowns = document.querySelectorAll("*[class='pv2']")
+        var injectedHtmls = document.querySelectorAll("*[id='injected']")
+        if (popdowns.length > 0 && injectedHtmls.length == 0) {
+            popdowns[0].insertAdjacentHTML("afterend", allowDeleteButtonHTML)
+            let justInjected = document.getElementById("injected")
+            justInjected.addEventListener("click", toggleDeleteEnabled)
+        }
+    }
 }
 
 var uid;
@@ -30,7 +50,7 @@ observer.observe(targetNode, { childList: true, subtree: true });
 safeSelectors = ["matters", "clients", "companies", "contacts"]
 
 function hideAllDeleteButtons() {
-    if (document.readyState != "loading") {
+    if (document.readyState != "loading" && !deleteEnabled) {
         // getUserCookie();
         // console.log(uid)
         for (var i = 0; i < safeSelectors.length; i++) {
@@ -43,6 +63,22 @@ function hideAllDeleteButtons() {
 
                 break;
             }
+        }
+    }
+}
+
+function makeMatterButtons() {
+    if (window.location.pathname.includes("matters") && document.readyState != "loading") {
+        var nodes = document.querySelectorAll("*[role='row']")
+        for (var i = 1; i < nodes.length; i++) {
+            let link = nodes[i].querySelector('*[data-cy="matter-details-link"]').href
+            nodes[i].style.cursor = "pointer"
+            nodes[i].addEventListener("click", function (e) {
+                if (e.target !== this) {
+                    return;
+                }
+                window.location.href = link
+            })
         }
     }
 }
